ab9ad251ba4de276d9933d9c2b8329110712df07
Willochs316 <willochs316@gmail.com>
2023-08-22 16:37:35 +0100

    refactor starred users component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import StarredUsers from './components/StarredUsers/StarredUsers';
+import StarredUsers from './component/StarredUsers/StarredUsers';
 import './App.css';
 
 const App = () => {
